e1d51f745a6c941c1345d5104aa6f48b6e84ea29
Erick Villa R <dexbatmon97@gmail.com>
2021-04-13 10:47:54 -0300

    scriptgoogle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mysolution';
+
+  ngOnInit(): void {
+  }
+
+  /*mounted(){
+    new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete")
+    )
+  }
+*/
 }
