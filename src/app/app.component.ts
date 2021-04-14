fc3a2ddee5bf1043a5c05789bd3cd595afe4714e
Erick Villa R <dexbatmon97@gmail.com>
2021-04-14 10:25:25 -0300

    inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,15 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  lat: any;
+  condition=false;
   title = 'mysolution';
+  public createTicketDisabled: boolean = true;
 
   ngOnInit(): void {
+    console.log("holi");
+
+  
+  }
+  
+  change(event : any) {
+    this.lat=event.target.value;
+    console.log(this.lat);}
+  
+  
+ 
+  
+  
+  
+  search(){
+    this.condition=!this.condition
+    //aqui va a hacer un HTTP post?
+     // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&keyword=cruise&key=AIzaSyAVeX4jmQihj8faM-t2uvAGJgILXymB_Rc
   }
 
-  /*mounted(){
+  /*
+  mounted(){
+    const directionsService = new google.maps.DirectionsService(); 
+
     new google.maps.places.Autocomplete(
       document.getElementById("autocomplete")
     )
-  }
-*/
+  }*/
+
 }
