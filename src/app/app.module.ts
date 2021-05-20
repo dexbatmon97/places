1bfe47674fca6232099b03bf14ac847c82e7b494
Erick Villa R <dexbatmon97@gmail.com>
2021-05-20 12:46:47 -0300

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { PlacesComponent } from './places/places.component';
     HttpClientModule,
     InfiniteScrollModule,
     AgmCoreModule.forRoot({
-      apiKey: ''
+      apiKey: 'mykey'
     })
   ],
   providers: [],
